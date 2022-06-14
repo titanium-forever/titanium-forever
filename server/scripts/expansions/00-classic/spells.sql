967d8bfaf5722c8f81190a3e3e1f832b65140800
titanium-forever <kieren@titanium-forever.uk>
2022-06-14 22:17:42 +0100

    set steamfont and lavastorm druid spells to correct coordinates for classic zones when using classic

---
+++ server/scripts/expansions/00-classic/spells.sql
@@ -0,0 +1,5 @@
+-- Set Circle of Lavastorm spell to correct coordinates for classic zone
+UPDATE spells_new SET effect_base_value1="1036",effect_base_value2="1386",effect_base_value3="129" WHERE id=554;
+
+-- Set Circle of Steamfont spell to correct coordinates for classic zone
+UPDATE spells_new SET effect_base_value1="-1856",effect_base_value2="1662",effect_base_value3="-106" WHERE id=557;
